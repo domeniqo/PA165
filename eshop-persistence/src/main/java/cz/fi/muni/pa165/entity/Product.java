900b404c6274a5b479ad3505dc2b8cdcfe70bb77
Dominik Mlynka <dominik.mlynka@gmail.com>
2017-10-12 01:33:06 +0200

    Task01 completed, task02 does not work

--- eshop-persistence/src/main/java/cz/fi/muni/pa165/entity/Product.java
+++ eshop-persistence/src/main/java/cz/fi/muni/pa165/entity/Product.java
@@ -68,30 +68,21 @@ public class Product {
 	}
 
 
-
-	/**
-	 * TODO these two methods are here just to make Task04 compilable. After you are finished
-	 * with TASK 02 you should delete this empty method
-	 * @param kitchen
-	 */
-	public void addCategory(Category kitchen) {	
-	}
-	public List<Product> getCategories() {
-		return null;
-	}
+        @ManyToMany
+        private Set<Category> categories;
 	//TODO after you are done with task02 you can uncomment this methods
-//	public void removeCategory(Category category)	{
-//		this.categories.remove(category);
-//	}
-//	
-//	public void addCategory(Category c) {
-//		categories.add(c);
-//		c.addProduct(this);
-//	}
-//
-//	public Set<Category> getCategories() {
-//		return Collections.unmodifiableSet(categories);
-//	}
+	public void removeCategory(Category category)	{
+		this.categories.remove(category);
+	}
+	
+	public void addCategory(Category c) {
+		categories.add(c);
+		c.addProduct(this);
+	}
+
+	public Set<Category> getCategories() {
+		return Collections.unmodifiableSet(categories);
+	}
 	
 
 
--- eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task02.java
+++ eshop-persistence/src/test/java/cz/fi/muni/pa165/tasks/Task02.java
@@ -23,8 +23,56 @@ public class Task02 extends AbstractTestNGSpringContextTests {
 
 	@PersistenceUnit
 	private EntityManagerFactory emf;
+        private Category kitchen;
+        private Category electro;
+        private Product flashlight;
+        private Product kitchenRobot;
+        private Product plate;
 
-	
+	@BeforeClass
+        private void initialize(){
+            EntityManager em = emf.createEntityManager();
+            kitchen = new Category();
+            kitchen.setName("Kitchen");
+            electro = new Category();
+            electro.setName("Electro");
+            
+            flashlight = new Product();
+            flashlight.setName("Flashlight");
+            flashlight.addCategory(electro);
+            
+            kitchenRobot = new Product();
+            kitchenRobot.setName("Kitchen robot");
+            kitchenRobot.addCategory(electro);
+            kitchenRobot.addCategory(kitchen);
+            
+            plate = new Product();
+            plate.setName("Plate");
+            plate.addCategory(kitchen);
+            
+            em.getTransaction().begin();
+            em.persist(kitchen);
+            em.persist(electro);
+            em.persist(kitchenRobot);
+            em.persist(flashlight);
+            em.persist(plate);
+            em.getTransaction().commit();
+            em.close();
+            
+        }
+        
+        @Test
+        private void kitchenCategoryTest(){
+            EntityManager em = emf.createEntityManager();
+            em.getTransaction().begin();
+            Category catA = em.find(Category.class, 1L);
+            em.close();
+            
+            assertContainsProductWithName(catA.getProducts(), "Kitchen robot");
+            assertContainsProductWithName(catA.getProducts(), "Plate");
+            
+        }
+        
 	private void assertContainsCategoryWithName(Set<Category> categories,
 			String expectedCategoryName) {
 		for(Category cat: categories){
